5955d98349aefc7a439e331b636d073f0dc2775d
Pokechu22 <Pokechu022@gmail.com>
2017-08-09 16:39:24 -0700

    Unescape HTML entities

--- src/main/java/me/urielsalis/mojiraFeedIRC/Feed.java
+++ src/main/java/me/urielsalis/mojiraFeedIRC/Feed.java
@@ -1,6 +1,7 @@
 package me.urielsalis.mojiraFeedIRC;
 
 import com.sun.syndication.feed.synd.SyndEntryImpl;
+import org.apache.commons.text.StringEscapeUtils;
 
 /**
  * mojirafeed
@@ -14,7 +15,7 @@ public class Feed {
 
     public Feed(SyndEntryImpl entry) {
         this.link = entry.getLink().replace("&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel", ""); // shorten comment links
-        this.title = entry.getTitle().replaceAll("\\<[^>]*>", "").trim().substring(entry.getAuthor().length()+1).replaceAll("\\s+", " "); //remove all html tags and extra spaces/new lines
+        this.title = StringEscapeUtils.unescapeHtml4(entry.getTitle().replaceAll("\\<[^>]*>", "").trim().substring(entry.getAuthor().length()+1).replaceAll("\\s+", " ")); //remove all html tags and extra spaces/new lines; unescape HTML entities
         this.author = entry.getAuthor().replaceAll("\\[[^\\]]*", "").trim().replaceAll(" ", "_"); // remove [Mod] prefixes; don't include spaces in usernames
     }
 
