8c605b213710ba8f523648a290018923d4239f80
Pokechu22 <Pokechu022@gmail.com>
2017-08-12 16:30:48 -0700

    Limit author names to 20 characters

--- src/main/java/me/urielsalis/mojiraFeedIRC/Feed.java
+++ src/main/java/me/urielsalis/mojiraFeedIRC/Feed.java
@@ -2,6 +2,7 @@ package me.urielsalis.mojiraFeedIRC;
 
 import com.sun.syndication.feed.synd.SyndEntryImpl;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * mojirafeed
@@ -16,7 +17,7 @@ public class Feed {
     public Feed(SyndEntryImpl entry) {
         this.link = entry.getLink().replace("&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel", ""); // shorten comment links
         this.title = StringEscapeUtils.unescapeHtml4(entry.getTitle().replaceAll("\\<[^>]*>", "").trim().substring(entry.getAuthor().length()+1).replaceAll("\\s+", " ")); //remove all html tags and extra spaces/new lines; unescape HTML entities
-        this.author = entry.getAuthor().replaceAll("\\[.*?\\]", "").trim().replaceAll(" ", "_"); // remove [Mod] prefixes; don't include spaces in usernames
+        this.author = StringUtils.abbreviate(entry.getAuthor().replaceAll("\\[.*?\\]", "").trim().replaceAll(" ", "_"), 20); // remove [Mod] prefixes; don't include spaces in usernames; make sure the length is no more than 20
     }
 
     @Override
